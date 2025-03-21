ee01ea77a0f5c52bb2d12e30c27dc88f426f7b5f
JaspreetSingh-exe <jaspreetsingh01110@gmail.com>
2025-03-21 22:41:46 +0530

    file changes commit

---
+++ diagnosis-agent/ReportHandlerAgent.py
@@ -0,0 +1,4 @@
+"""
+File for agent which takes the query from the user and forwards it to the required agent.
+
+"""
---
+++ diagnosis-agent/ReportSummarizerAgent.py
@@ -0,0 +1,3 @@
+"""
+File for agent which takes text reports and images,  finds the anomalies and give the summary.
+"""
